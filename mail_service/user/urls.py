7e07b6179dba6adb8864e940752ebe6fb4c7400e
Codirovshik <block-code@mail.ru>
2021-05-31 13:43:15 +0300

    пока убрал настройки профиля и добавил страницу контакты

--- mail_service/user/urls.py
+++ mail_service/user/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from .views import index, login_view, registration_view, logout_view, profile_view, profile_settings_view, \
-    user_contacts_view
+from .views import index, login_view, registration_view, logout_view, profile_view, user_contacts_view
 
 urlpatterns = [
     path('', index, name='index'),
@@ -9,6 +8,6 @@ urlpatterns = [
     path('logout', logout_view, name='logout'),
     path('registration', registration_view, name='registration'),
     path('profile', profile_view, name='profile'),
-    path('profile/settings', profile_settings_view, name='settings'),
+    # path('profile/settings', profile_settings_view, name='settings'),
     path('contacts', user_contacts_view, name='contacts'),
 ]
--- mail_service/user/views.py
+++ mail_service/user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.hashers import make_password
 
 
 def check_att(cur_att, new_att):
@@ -11,12 +11,14 @@ def check_att(cur_att, new_att):
     return cur_att
 
 
-@login_required
 def index(request):
-    res = False
     if request.user.is_authenticated:
-        res = True
-    return render(request, 'base.html', {'auth': res})
+        res = False
+        if request.user.is_authenticated:
+            res = True
+        return render(request, 'base.html', {'auth': res})
+    else:
+        return redirect('login')
 
 
 def login_view(request):
@@ -34,8 +36,11 @@ def login_view(request):
 
 
 def logout_view(request):
-    logout(request)
-    return redirect('index')
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect('index')
+    else:
+        return redirect('login')
 
 
 def registration_view(request):
@@ -52,25 +57,41 @@ def registration_view(request):
     return render(request, 'registration.html')
 
 
-@login_required
 def profile_view(request):
-    cur_user = request.user
-    return render(request, 'profile.html', {'user': cur_user})
+    if request.user.is_authenticated:
+        cur_user = request.user
+        return render(request, 'profile.html', {'user': cur_user})
+    else:
+        return redirect('login')
 
 
-@login_required
-def profile_settings_view(request):
-    cur_user = request.user
-    if request.method == 'POST':
-        username = request.POST.get('name')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+# def profile_settings_view(request):
+#     if request.user.is_authenticated:
+#         cur_user = User.objects.get(username=request.user.username)
+#         if request.method == 'POST':
+#             new_username = request.POST.get('name')
+#             new_email = request.POST.get('new_email')
+#             new_password = request.POST.get('password')
+#
+#             cur_user.username = check_att(cur_att=cur_user.username, new_att=new_username)
+#             cur_user.email = check_att(cur_att=cur_user.email, new_att=new_email)
+#
+#             cur_user.password = make_password(new_password)
+#             cur_user.save()
+#
+#             cur_user = User.objects.get(username=request.user.username)
+#             user = authenticate(request, username=cur_user.username, password=cur_user.password)
+#
+#             login(request, user)
+#             return redirect('settings')
+#
+#         return render(request, 'settings.html', {'user': cur_user})
+#     else:
+#         return redirect('login')
 
-        cur_user.username = check_att(cur_att=cur_user.username, new_att=username)
 
-    return render(request, 'settings.html', {'user': cur_user})
-
-
-@login_required
 def user_contacts_view(request):
-    return render(request, 'contacts.html')
+    if request.user.is_authenticated:
+        return render(request, 'contacts.html')
+    else:
+        return redirect('login')
